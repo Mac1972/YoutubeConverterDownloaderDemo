45ec7bebc678a44741dd168d8aea26e142f2829d
Mac <honeyhome1972@yahoo.co.jp>
2019-05-10 05:30:00 +0800

    ok

--- pytubeTest1.py
+++ pytubeTest1.py
@@ -2,6 +2,6 @@ from pytube import YouTube
 
 link= '.............'
 yt=YouTube(link)
-video=yt.streams.filter(file_extension='mp4', res='144p').first()
+video=yt.streams.filter(file_extension='mp4', res='1080p').first()
 video.download()
 
