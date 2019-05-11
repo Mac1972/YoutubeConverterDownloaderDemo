8acbd85309172a9c42a00c03e73fc1a0ffa186a9
MacLin <50097920+Mac1972@users.noreply.github.com>
2019-05-12 00:38:11 +0800

    Update pytubeTest1.py

--- pytubeTest1.py
+++ pytubeTest1.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 
-link= 'https://.......'
-yt=YouTube(link)
-video=yt.streams.filter(file_extension='mp4', res='1080p').first()
+link = 'https://.......'
+yt = YouTube(link)
+video = yt.streams.filter(file_extension='mp4', res='1080p').first()
 video.download()
 
